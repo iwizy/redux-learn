3ee448fc86493c8e05d669e4e9feb3e5c2969a74
Alexander Agafonov <agafonov.alexander@gmail.com>
2020-03-14 18:28:36 +0300

    Source app structure

---
+++ src/app/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        hello world!
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
