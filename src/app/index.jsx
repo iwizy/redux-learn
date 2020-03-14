30c73eb4af63561251de64eb012c119042532981
Alexander Agafonov <agafonov.alexander@gmail.com>
2020-03-14 22:09:59 +0300

    Import react-dom to root file and add logic to app

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 import './style.css';
 
 class App extends Component {
+  state = {
+    count: 0
+  };
+
+  increment = (event) => {
+    let {count} = this.state;
+    this.setState({
+      count: this.state.count + 1
+    })
+  };
+
+  decrement = (event) => {
+    let {count} = this.state;
+    this.setState({
+      count: this.state.count - 1
+    })
+  };
+
   render() {
     return (
       <div>
-        hello world!
+        <div>{this.state.count}</div>
+        <div>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+        </div>
       </div>
     );
   }
